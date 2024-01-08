9fbddaa595253ad5334e247c19a39410ee1f230e
jadeilton <jadeiltondm@gmail.com>
2024-01-08 14:35:00 -0300

    feat: Modificando uma coisinha no código do cabeça

--- src/test/java/com/example/demo/service/impl/PautaServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/PautaServiceImplTest.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @SpringBootTest
 @AutoConfigureMockMvc
 class PautaServiceImplTest {
-
     @Autowired
     private MockMvc mockMvc;
     @Test
